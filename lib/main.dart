8ea16c41fe113b28f73458e3ff383415a0f10355
keyband <keybpr@gmail.com>
2021-01-12 20:16:07 -0300

    Changed title, but the Github page seems off

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Escada Games',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.black,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
