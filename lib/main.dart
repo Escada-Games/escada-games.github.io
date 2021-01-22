b6b1dc76ddf1310c5ae8a40ece2ab885a76e69f4
keyband <keybpr@gmail.com>
2021-01-22 16:04:11 -0300

    Now anywhere in the GameWidget can be clicked to open the link

--- lib/main.dart
+++ lib/main.dart
@@ -255,56 +255,74 @@ class _GameWidgetState extends State<GameWidget> {
   @override
   Widget build(BuildContext context) {
     return AnimatedContainer(
-      duration: const Duration(milliseconds: 200),
-      transform: _bHovering ? widget.hoverTransform : widget.nonHoverTransform,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16.0),
-        ),
-        margin: EdgeInsets.all(8),
-        elevation: 8,
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 8.0,
-              ),
-              SelectableText(widget.strTitle,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                  )),
-              ClipRRect(
+        duration: const Duration(milliseconds: 200),
+        transform:
+            _bHovering ? widget.hoverTransform : widget.nonHoverTransform,
+        child: MouseRegion(
+          onEnter: (e) {
+            setState(() {
+              _bHovering = true;
+            });
+          },
+          onExit: (e) {
+            setState(() {
+              _bHovering = false;
+            });
+          },
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: () {
+              _launchURL(widget.strGameUrl);
+            },
+            child: Card(
+              shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(16.0),
-                child: MouseRegion(
-                  onEnter: (e) {
-                    setState(() {
-                      _bHovering = true;
-                    });
-                  },
-                  onExit: (e) {
-                    setState(() {
-                      _bHovering = false;
-                    });
-                  },
-                  cursor: SystemMouseCursors.click,
-                  child: GestureDetector(
-                    onTap: () {
-                      _launchURL(widget.strGameUrl);
-                    },
-                    child: Image.network(
-                      widget.strCoverImageUrl,
-                      width: 315,
-                      alignment: Alignment.center,
-                    ),
-                  ),
-                ),
               ),
-            ]),
-      ),
-    );
+              margin: EdgeInsets.all(8),
+              elevation: 8,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    SelectableText(widget.strTitle,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        )),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(16.0),
+                      child: MouseRegion(
+                        onEnter: (e) {
+                          setState(() {
+                            _bHovering = true;
+                          });
+                        },
+                        onExit: (e) {
+                          setState(() {
+                            _bHovering = false;
+                          });
+                        },
+                        cursor: SystemMouseCursors.click,
+                        child: GestureDetector(
+                          onTap: () {
+                            _launchURL(widget.strGameUrl);
+                          },
+                          child: Image.network(
+                            widget.strCoverImageUrl,
+                            width: 315,
+                            alignment: Alignment.center,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
+          ),
+        ));
   }
 }
 
