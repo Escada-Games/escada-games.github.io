93202b8d3c1ace4bad9c179ec0e169f7615a0d37
keyband <keybpr@gmail.com>
2021-02-05 15:16:14 -0300

    Commented out a line I forgot to comment out before

--- lib/main.dart
+++ lib/main.dart
@@ -210,7 +210,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     //   onPressed: () {
                     //     ;
                     //   },
-                    ),
+                    // ),
                     SizedBox(
                       width: 16,
                     )
